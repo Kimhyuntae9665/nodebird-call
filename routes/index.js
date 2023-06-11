b067c23a773d5e14d4820777fd66beb03f5c045e
Kimhyuntae9665 <Kimhyuntae9665>
2023-06-11 13:22:44 +0900

    뭐가 문제인지

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,19 @@ const router = express.Router();
 router.get('/test', async (req, res, next) => {
   try {
     if (!req.session.jwt) { // 세션에 토큰이 없으면
+      console.log("여기까지는 도착: !");
+
       const tokenResult = await axios.post('http://localhost:8002/v1/token', {
         clientSecret: process.env.CLIENT_SECRET,
       });
+
+      await console.log("이게 발급받은 토큰 ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ: "+ tokenResult);
+
       if (tokenResult.data && tokenResult.data.code === 200) { // 토큰 발급 성공
+        console.log("여기는 성공")
         req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
       } else { // 토큰 발급 실패
+        console.log("여기가 실패");
         return res.json(tokenResult.data); // 발급 실패 사유 응답
       }
     }
